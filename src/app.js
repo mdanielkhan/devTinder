70fe9f894754bc5af4bdcf0a01dc5b91e076c3aa
Daniyal Khan <mdanielkhan2@gmail.com>
2026-05-25 15:49:01 +0500

    Post API for database

--- src/app.js
+++ src/app.js
@@ -1,58 +1,40 @@
 const express = require("express");
-// app.js
-const { adminmiddleware, usermiddleware } = require("./utils/middlewares");
+const connectDB = require("./config/database");
 const app = express();
+const UserModel =require("./models/user");
 
 
+app.post("/signup", async (req,res)=>{
+    try{
 
-app.use("/",(err, req, res, next) => {
-    if (err) {
-    console.error(err.stack);
-    res.status(500).send("Something broke!");
-  }
-
-});
-
-app.use("/admin", adminmiddleware) 
-
-
-
-
-app.get("/admin/data",  (req, res,next) => {
-   
-    res.send("Admin Data Accessed")
- 
-    
-});
-app.delete("/admin/delete", (req, res,next) => 
-{
-
-    res.send("Admin Delete Accessed")
-
-})
-app.post("/user/login",(req,res,next)=>
-{
-    res.send("User Login Accessed")
-})
-
-app.use("/user",usermiddleware)
-
-
-
-
-app.get("/user/data", (req, res,next) => {
-    res.send("User Data Accessed")
-})
-
-app.delete("/user/delete",  (req, res,next) => {
-    res.send("User delete Accessed")
+        const User=new UserModel({
+            firstName:"John",
+            lastName:"Doe",
+            email:"johndoe@example.com",
+            age:30,
+            password:"password123"
+        })
+        await User.save();
+        res.status(201).json({message:"User created successfully",User})
+    }catch(err){
+        res.status(500).json({message:"Internal server error",error:err.message})
+    }
 })
 
 
 
+connectDB() 
+    .then(()=>{  
+        console.log("Database connected successfully"),
+        app.listen(6969,()=>
+        {
+            console.log("Server is running on port 6969");
+        })
+    })
+    .catch((err)=>{
+        console.error("Database connection failed",err);
+    });
 
 
 
-app.listen(6969, () => {
-    console.log("Server's up and Running at 6969 port!");
-});
+
---
+++ src/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    age: {
+        type: Number,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
